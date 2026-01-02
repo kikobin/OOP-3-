b626ebaffdeb80d38171bc74d01f3b9e4e542b89
kiko <kiko.binary@gmail.com>
2026-01-02 11:07:07 +0500

    Painting

--- src/Painting.java
+++ src/Painting.java
@@ -1,2 +1,21 @@
-public class Painting {
+public class Painting extends Artwork {
+    private String technique;
+
+    public Painting(int id, String title, int year, double price, Artist artist, String technique) {
+        super(id, title, year, price, artist);
+        this.technique = technique;
+    }
+
+    public String getTechnique() { return technique; }
+    public void setTechnique(String technique) { this.technique = technique; }
+
+    @Override
+    public String getType() {
+        return "Painting";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}", ", technique='" + technique + "'}");
+    }
 }
