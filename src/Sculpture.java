0cb20ec6c5751e2f35bab29714f4f81d14cc953f
kiko <kiko.binary@gmail.com>
2026-01-02 11:07:28 +0500

    Sculpture

--- src/Sculpture.java
+++ src/Sculpture.java
@@ -1,2 +1,21 @@
-public class Sculpture {
+public class Sculpture extends Artwork {
+    private String material;
+
+    public Sculpture(int id, String title, int year, double price, Artist artist, String material) {
+        super(id, title, year, price, artist);
+        this.material = material;
+    }
+
+    public String getMaterial() { return material; }
+    public void setMaterial(String material) { this.material = material; }
+
+    @Override
+    public String getType() {
+        return "Sculpture";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("}", ", material='" + material + "'}");
+    }
 }
